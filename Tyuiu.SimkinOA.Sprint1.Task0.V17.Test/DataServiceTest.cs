d911e03a1a7966ea79ebde89a1348300f6d222b1
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-10-02 16:24:01 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.SimkinOA.Sprint1.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SimkinOA.Sprint1.Task0.V17.Lib;
+namespace Tyuiu.SimkinOA.Sprint1.Task0.V17.Test
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(5, res);
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint1.Task0.V17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Tyuiu.SimkinOA.Sprint1.Task0.V17.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Симкин О. А. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #17                                                             *");
+Console.WriteLine("* Выполнил: Симкин О. А. | РППб-25-1                                  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет выражение 9/3*2+12/4/3-2 и        *");
+Console.WriteLine("* печатает  результат на экране.                                          *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("9 / 3 * 2 + 12 / 4 / 3 - 2");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+
+Console.WriteLine(ds.Calculate());
+
+Console.ReadLine();
