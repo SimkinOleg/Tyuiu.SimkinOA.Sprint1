3dc9181afcc0bb661e7e6923c1ff6868c7bdf59e
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-10-02 20:26:52 +0500

    Task1

---
+++ Tyuiu.SimkinOA.Sprint1.Task1.V20/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.SimkinOA.Sprint1.Task1.V20.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Симкин О. А. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #1                                                              *");
+Console.WriteLine("* Вариант #20                                                             *");
+Console.WriteLine("* Выполнил: Симкин О. А. | РППб-25-1                                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет выражение 9/3*2+12/4/3-2 и        *");
+Console.WriteLine("* печатает  результат на экране.                                          *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+double x, y;
+
+Console.WriteLine("Введите значение X:");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение Y:");
+y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+
+Console.WriteLine(ds.Calculate(x, y));
+
+Console.ReadLine();
