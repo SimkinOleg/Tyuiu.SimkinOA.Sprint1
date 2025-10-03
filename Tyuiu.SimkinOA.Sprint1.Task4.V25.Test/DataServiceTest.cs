e8a7d89538ee37ad103696dc7174d9d37843d62f
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-10-03 20:49:10 +0500

    Task4

---
+++ Tyuiu.SimkinOA.Sprint1.Task4.V25.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SimkinOA.Sprint1.Task4.V25.Lib;
+namespace Tyuiu.SimkinOA.Sprint1.Task4.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double a = 0;
+            var res = ds.Calculate(a);
+            Assert.AreEqual(res,0);
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint1.Task4.V25/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.SimkinOA.Sprint1.Task4.V25.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Симкин О. А. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #25                                                             *");
+Console.WriteLine("* Выполнил: Симкин О. А. | РППб-25-1                                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет значение по                       *");
+Console.WriteLine("* формуле и печатает его на экране                                        *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+double number;
+Console.WriteLine("Введите значение в градусах : ");
+number = double.Parse(Console.ReadLine());
+
+double result = ds.Calculate(number);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine(result);
