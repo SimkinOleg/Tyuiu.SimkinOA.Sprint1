8500092de78cf90cdc4e6bb3b4b4270432beeba9
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-10-03 20:22:33 +0500

    Task3

---
+++ Tyuiu.SimkinOA.Sprint1.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.SimkinOA.Sprint1.Task3.V3.Lib;
+namespace Tyuiu.SimkinOA.Sprint1.Task3.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x  = 9;
+            double y = 7.5;
+            double z = 5;
+            var res = ds.ParallelepipedVolume(x,y,z);
+            Assert.AreEqual(337,5, res);
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint1.Task3.V3/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.SimkinOA.Sprint1.Task3.V3.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Симкин О. А. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #3                                                              *");
+Console.WriteLine("* Выполнил: Симкин О. А. | РППб-25-1                                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, вычисляет объём параллелепипеда и                   *");
+Console.WriteLine("* печатает  результат на экране.                                          *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+double length, width, height;
+
+Console.WriteLine("Введите длину: ");
+length = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите ширину: ");
+width = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите высоту: ");
+height = double.Parse(Console.ReadLine());
+
+double result = ds.ParallelepipedVolume(length, width, height);
+
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine(result);
